4aed85f0a3c8ee060daa104bdf8dbd1e50d770b9
kapil-inteior <kapilinterior36@gmail.com>
2025-05-25 19:23:40 +0530

    fix: mobile number was removed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,38 +55,7 @@ export default function Contact() {
             >
               Interior | Architecture | Turnkey
             </motion.p>
-            <motion.div
-              className="flex items-start"
-              initial={{ opacity: 0 }}
-              whileInView={{ opacity: 1 }}
-              viewport={{ once: true, amount: 0.3 }}
-              transition={{ duration: 0.5, delay: 0.3, ease: "easeOut" }}
-            >
-              <div className="mr-4 mt-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={1}
-                    d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                  />
-                </svg>
-              </div>
-              <div>
-                <a
-                  href="tel:+918861636827"
-                  className="text-gray-300 hover| hover:text-white"
-                >
-                  +91 8861636827
-                </a>
-              </div>
-            </motion.div>
+         
           </motion.div>
 
           {/* Second column: Contact details */}
