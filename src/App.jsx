46d4a5445d61bb3338ab4317de16ec3394965e61
kapil-inteior <kapilinterior36@gmail.com>
2025-06-19 21:44:33 +0530

    fix: fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,64 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LandingPage from "./components/LandingPage";
-// import ServicesSection from "./components/ServicesSection";
-// import Contact from "./components/Contact";
-// import AllProjects from "./components/AllProjects";
-// import Navbar from "./components/Navbar";
-// import RecentProjects from "./components/RecentProjects";
-// import Testimonial from "./components/Testimonial";
-// import Bathroom from "./components/Services/Bathroom";
-// import Crockery from "./components/Services/Crockery";
-// import FalseCeiling from "./components/Services/FalseCeiling";
-// import Light from "./components/Services/Light";
-// import ModularKitchen from "./components/Services/ModularKitchen";
-// import SpaceSaving from "./components/Services/SpaceSaving";
-// import Storage from "./components/Services/Storage";
-// import StudyTable from "./components/Services/StudyTable";
-// import TvUnits from "./components/Services/TvUnits";
-// import WallPaint from "./components/Services/WallPaint";
-// import Wallpaper from "./components/Services/Wallpaper";
-// import Gallery from "./components/Gallery";
-// import Card from "./components/Card";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import ServicesSection from "./components/ServicesSection";
+import Contact from "./components/Contact";
+import AllProjects from "./components/AllProjects";
+import Navbar from "./components/Navbar";
+import RecentProjects from "./components/RecentProjects";
+import Testimonial from "./components/Testimonial";
+import Bathroom from "./components/Services/Bathroom";
+import Crockery from "./components/Services/Crockery";
+import FalseCeiling from "./components/Services/FalseCeiling";
+import Light from "./components/Services/Light";
+import ModularKitchen from "./components/Services/ModularKitchen";
+import SpaceSaving from "./components/Services/SpaceSaving";
+import Storage from "./components/Services/Storage";
+import StudyTable from "./components/Services/StudyTable";
+import TvUnits from "./components/Services/TvUnits";
+import WallPaint from "./components/Services/WallPaint";
+import Wallpaper from "./components/Services/Wallpaper";
+import Gallery from "./components/Gallery";
+import Card from "./components/Card";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             <>
-//               <LandingPage />
-//               <RecentProjects />
-//               <Card/>
-//               <ServicesSection />
-//               <Testimonial />
-//               <Contact />
-//             </>
-//           }
-//         />
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <LandingPage />
+              <RecentProjects />
+              <Card/>
+              <ServicesSection />
+              <Testimonial />
+              <Contact />
+            </>
+          }
+        />
 
-//         <Route path="/projects" element={<AllProjects />} />
-//         <Route path="/gallery" element={<Gallery />} />
-//         <Route path="/services/bathroom" element={<Bathroom />} />
-//         <Route path="/services/crockery-units" element={<Crockery />} />
-//         <Route path="/services/false-ceiling" element={<FalseCeiling />} />
-//         <Route path="/services/light" element={<Light />} />
-//         <Route path="/services/modular-kitchen" element={<ModularKitchen />} />
-//         <Route
-//           path="/services/space-saving-furniture"
-//           element={<SpaceSaving />}
-//         />
-//         <Route path="/services/storage-&-wardrobe" element={<Storage />} />
-//         <Route path="/services/study-tables" element={<StudyTable />} />
-//         <Route path="/services/tv-units" element={<TvUnits />} />
-//         <Route path="/services/wall-paint" element={<WallPaint />} />
-//         <Route path="/services/wallpaper" element={<Wallpaper />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+        <Route path="/projects" element={<AllProjects />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/services/bathroom" element={<Bathroom />} />
+        <Route path="/services/crockery-units" element={<Crockery />} />
+        <Route path="/services/false-ceiling" element={<FalseCeiling />} />
+        <Route path="/services/light" element={<Light />} />
+        <Route path="/services/modular-kitchen" element={<ModularKitchen />} />
+        <Route
+          path="/services/space-saving-furniture"
+          element={<SpaceSaving />}
+        />
+        <Route path="/services/storage-&-wardrobe" element={<Storage />} />
+        <Route path="/services/study-tables" element={<StudyTable />} />
+        <Route path="/services/tv-units" element={<TvUnits />} />
+        <Route path="/services/wall-paint" element={<WallPaint />} />
+        <Route path="/services/wallpaper" element={<Wallpaper />} />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
